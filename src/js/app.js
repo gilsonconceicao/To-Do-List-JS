a1d13f1a33842c85b66870e7ca512f75a4822c94
gilsonconceicao <gilsonsantosjunior02@gmail.com>
2022-08-08 14:11:12 -0300

    version finish

--- src/js/app.js
+++ src/js/app.js
@@ -1,92 +1,94 @@
-const titleTaskForm = document.querySelector('#nameOfProdut').value; 
-const textDescritionForm = document.querySelector('#textDescrition').value; 
-const listProductPage = document.querySelector('.listProducts')
-const bankData = []; 
+const listProductPage = document.querySelector(".listProducts");
+const bankData = [];
 
 class List {
-    constructor(titleTask, descritionTask) {
-        this.titleTask = titleTask; 
-        this.descritionTask = descritionTask; 
+  constructor(titleTask, descritionTask) {
+    this.titleTask = titleTask;
+    this.descritionTask = descritionTask;
+  }
+
+  addList() {
+    // submit array
+    const newProduct = {
+      title: this.titleTask,
+      descrition: this.descritionTask,
+    };
+
+    bankData.push(newProduct);
+    localStorage.setItem("List", JSON.stringify(newProduct));
+    // add data hours
+
+    let currentDay = "";
+    let currentMonth = "";
+
+    if (currentDay <= 10) {
+      currentDay = "0" + new Date().getDate();
     }
-    
-    addList () {
-        // submit array 
-        const newProduct = {
-            title: this.titleTask, 
-            descrition: this.descritionTask
-        }
+    if (currentMonth <= 10) {
+      currentMonth = "0" + new Date().getMonth();
+    }
 
-        bankData.push(newProduct); 
-        localStorage.setItem('List', JSON.stringify(newProduct));        
-        // add data hours
+    const dateComplete = `${currentDay}/${currentMonth}/${new Date().getFullYear()}`;
 
-        let currentDay = '';
-        let currentMonth = '';
-        
-        if (currentDay <= 10) {
-            currentDay = '0' + new Date().getDate(); 
-        } 
-        if (currentMonth <= 10) {
-            currentMonth = '0' + new Date().getMonth();
-        }
+    // contentText
+    const bodyInfo = document.createElement("div");
+    bodyInfo.setAttribute("class", "product-users");
+    bodyInfo.setAttribute("remover", "remover");
 
-        const dateComplete = `${currentDay}/${currentMonth}/${new Date().getFullYear()}`
+    //create add info
 
-        // contentText
-        const bodyInfo = document.createElement('div');
-        bodyInfo.setAttribute('class', 'product-users'); 
-        bodyInfo.setAttribute('remover', 'remover');         
-
-        //create add info 
-
-        bodyInfo.innerHTML = `
-            <h2> ${newProduct.title}</h2>
-            <p> ${newProduct.descrition}</p>
+    bodyInfo.innerHTML = `
+            <h2> ${this.titleTask}</h2>
+            <p> ${this.descritionTask}</p>
             <span>
             <span class='dateList'>${dateComplete}</span>
             <button removerBtn>
                 <i class="fa-solid fa-trash-can"></i>
             </button>
             </span>
-            `
-        
-        listProductPage.appendChild(bodyInfo); 
+            </br>
+            `;
 
-        // clear
-        titleTaskForm.innerHTML = '';
-        textDescritionForm.innerHTML = ''; 
-    }
+    listProductPage.appendChild(bodyInfo);
 
-    removeItemList() {
-        document.querySelectorAll('[removerBtn]').forEach(itemButton => 
-            itemButton.onclick = function() {
+  }
 
-                document.querySelectorAll('[remover]').forEach(itemButton => 
-                    itemButton.onclick = function() {
-                        alert('Um item foi removido com sucesso.')
-                        itemButton.style.display = 'none'
-                    })
-            })
-    }
-
+  removeItemList() {
+    document.querySelectorAll("[removerBtn]").forEach(
+      (itemButton) =>
+        (itemButton.onclick = function () {
+          document.querySelectorAll("[remover]").forEach(
+            (itemButton) =>
+              (itemButton.onclick = function () {
+                alert("Um item foi removido com sucesso.");
+                itemButton.style.display = "none";
+              })
+          );
+        })
+    );
+  }
 }
-
 
 function btnAddSave() {
 
-    if (titleTask === '' && textDescrition === '') {
-        alert('Digite os dois campos para prosseguir...');
-    } else if (titleTask === '' ) {
-        alert('[Erro] - Titúlo não inserido!');
-
-    } else if (textDescrition === '') {
-        alert('[Erro] - Descrição não inserida!');
-
+  const titleTaskForm = document.querySelector("#nameOfProdut").value;
+  const textDescritionForm = document.querySelector("#textDescrition").value;
+  const messageerror = document.querySelector('.textError'); 
+  messageerror.style.color = 'red'
+  
+  if (titleTaskForm === '' && textDescritionForm === '') {
+      messageerror.innerHTML = 'Os dados não estão preenchidos!'
+    } else if (titleTaskForm == '') {
+        messageerror.innerHTML = 'Digite o titulo para prosseguir...'
+    } else if (textDescritionForm == '') {
+        messageerror.innerHTML = 'Digite a descrição para prosseguir...'
     } else {
+        //invoca
+    const instanciar = new List(titleTaskForm, textDescritionForm);
+    instanciar.addList();
+    instanciar.removeItemList();
+    messageerror.innerHTML = 'Salvo com sucesso'; 
+    messageerror.style.color = 'green'
+  } 
 
-        const instanciar = new List(titleTaskForm, textDescritionForm); 
-
-        instanciar.addList();
-        instanciar.removeItemList();
-    }
 }
